3f1d46a5a8d00f141bc9bde8a5bc8ef5b680f945
williford-abunas <abunaswilliford@gmail.com>
2024-06-20 22:40:54 +1200

    clone backend notes p3

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const express = require('express')
+import express from 'express'
+import notesData from './notesData.js'
+
 const app = express()
+const PORT = process.env.PORT || 3001
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
-
+//MIDDLEWARE
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -29,9 +12,8 @@ app.get('/', (req, res) => {
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
+  const maxId =
+    notesData.length > 0 ? Math.max(...notesData.map((n) => n.id)) : 0
   return maxId + 1
 }
 
@@ -39,8 +21,8 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing',
     })
   }
 
@@ -51,25 +33,25 @@ app.post('/api/notes', (request, response) => {
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  notesData.push(note)
 
   response.json(note)
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  res.json(notesData)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  notes = notesData.filter((note) => note.id !== id)
 
   response.status(204).end()
 })
 
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+  const note = notesData.find((note) => note.id === id)
 
   if (note) {
     response.json(note)
@@ -78,7 +60,6 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
