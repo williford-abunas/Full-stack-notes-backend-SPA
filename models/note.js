82f54734c28e4738dcecbba2726cd4d80cfa14ad
williford-abunas <abunaswilliford@gmail.com>
2024-07-05 20:26:15 +1200

    integrate mongoDB to backend

---
+++ models/note.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URL
+console.log('connecting to', url)
+
+// DB connection
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+export default mongoose.model('Note', noteSchema)
--- routes/notes.js
+++ routes/notes.js
@@ -1,28 +1,35 @@
 import express from 'express'
-import notesData from '../notesData.js'
-import { generateId } from '../utils.js'
+import Note from '../models/note.js'
 
 export const notesRouter = express.Router()
 
 // GET all notes
-notesRouter.get('/notes', (req, res) => {
-  res.json(notesData)
-})
-
-// GET single note by id
-notesRouter.get('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notesData.find((note) => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
+notesRouter.get('/notes', async (req, res) => {
+  try {
+    const notes = await Note.find({})
+    res.json(notes)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server Error')
   }
 })
 
-// POST create new note in server
-notesRouter.post('/notes', (request, response) => {
+// GET single note by id
+notesRouter.get('/notes/:id', async (request, response) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    response.status(400).send({ error: error.message })
+  }
+})
+
+// POST create new note in MONGO DB
+notesRouter.post('/notes', async (request, response) => {
   const body = request.body
 
   if (!body.content) {
@@ -31,39 +38,49 @@ notesRouter.post('/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notesData.push(note)
-  response.json(note)
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch (error) {
+    console.error(error)
+    response.status(500).send('Server Error')
+  }
 })
 
 // DELETE single note by id
-notesRouter.delete('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const index = notesData.findIndex((note) => note.id === id)
-  if (index !== -1) notesData.splice(index, 1)
+notesRouter.delete('/notes/:id', async (request, response) => {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(request.params.id)
 
-  response.status(204).end()
+    if (!deletedNote)
+      return response.status(404).json({ error: 'Note not found' })
+    response.status(204).end()
+  } catch (error) {
+    response.status(400).send({ error: error.message })
+  }
 })
 
 // UPDATE existing note
-notesRouter.put('/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const body = request.body
+notesRouter.put('/notes/:id', async (request, response) => {
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true, runValidators: true }
+    )
 
-  const noteIndex = notesData.findIndex((note) => note.id === id)
-  if (noteIndex === -1) {
-    return response.status(404).json({ error: 'note not found' })
+    if (!updatedNote) {
+      return response.status(404).json({ error: 'Note not found' })
+    }
+
+    response.json(updatedNote)
+  } catch (error) {
+    response.status(400).send({ error: error.message })
   }
-
-  const updatedNote = { ...notesData[noteIndex], ...body, id }
-
-  notesData[noteIndex] = updatedNote
-
-  response.json(updatedNote)
 })
